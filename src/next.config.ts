82a2b3262e6b0cfca6c189bae371018d99a93adb
Prashant Pandey <officialprashant.org@gmail.com>
2025-07-30 10:46:53 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.ts
@@ -0,0 +1,27 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+  },
+  allowedDevOrigins: ['https://*.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev'],
+};
+
+export default nextConfig;
